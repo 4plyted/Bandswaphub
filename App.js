6d553fc2a98ead2462171b355fb389b4622fb79a
4plyted <Tedclarkmusic@gmail.com>
2025-07-13 22:27:15 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🎸 Welcome to BandSwapHub</h1>
+      <p>This is your launch-ready React frontend.</p>
+    </div>
+  );
+}
+
+export default App;
