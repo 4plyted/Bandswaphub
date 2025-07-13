087660b8e6e698b057be7bca0c3a6bcabcc3a718
4plyted <Tedclarkmusic@gmail.com>
2025-07-13 22:46:22 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🎸 Welcome to BandSwapHub</h1>
-      <p>This is your launch-ready React frontend.</p>
+      <p>This is your launch-ready React frontend with everything working!</p>
     </div>
   );
 }
