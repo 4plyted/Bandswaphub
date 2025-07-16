30fdbe951db50ee6e263a567cffb539ed1b5dd09
4plyted <Tedclarkmusic@gmail.com>
2025-07-16 23:45:53 +0100

    Update index.js

--- src/src/pages/index.js
+++ src/src/pages/index.js
@@ -1,1 +1,9 @@
 
+export default function Home() {
+  return (
+    <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", background: "#111", color: "#fff" }}>
+      <h1 style={{ fontSize: "3rem", fontWeight: "bold" }}>🎸 Welcome to BandSwapHub</h1>
+      <p style={{ marginTop: "1rem", fontSize: "1.2rem" }}>Visit /register to sign up your band</p>
+    </div>
+  );
+}
