dd31dffc922332dd6f884362943333a6f80f4981
4plyted <Tedclarkmusic@gmail.com>
2025-07-21 21:28:02 +0100

    Update firebaseConfig. 123.js

--- firebaseConfig. 123.js
+++ firebaseConfig. 123.js
@@ -1,15 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBXKSXFSWIFR1RVKkBNbdJPHT3orfN3FLs",
-  authDomain: "bandswaphub.firebaseapp.com",
-  projectId: "bandswaphub",
-  storageBucket: "bandswaphub.firebasestorage.app",
-  messagingSenderId: "513110553973",
-  appId: "1:513110553973:web:36d2535dcf4576b068cf3c"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
