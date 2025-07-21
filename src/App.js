40907c7b26156500a7e41bbcfed41e830c064cbe
4plyted <Tedclarkmusic@gmail.com>
2025-07-21 22:25:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./LandingPage";
+import HomePage from "./pages/index";
 import RegisterPage from "./pages/register";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
+        {/* Add more routes here */}
       </Routes>
     </Router>
   );
